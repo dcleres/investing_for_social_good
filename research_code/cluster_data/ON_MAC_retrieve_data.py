7cc9595fd679eccfe53c8f11ffa8d5b747e06982
David Cleres <davidcleres@gmail.com>
2018-12-14 04:23:52 +0100

    removed large file

--- research_code/cluster_data/ON_MAC_retrieve_data.py
+++ research_code/cluster_data/ON_MAC_retrieve_data.py
@@ -21,33 +21,34 @@ for k in range(1, 13):
 
     #strats at one since the is no day 0 in a month
     for j in range(1, 32):
-        if j < 10 :
-            day = '0' + str(j)
-        else:
-            day = str(j)
-
-        os.system('mkdir ' + month + '/' + day)
-
-        for i in range(24):
-            if i < 10 :
-                hour = '0' + str(i)
+        if j == 1 or j == 11 or j ==21:
+            if j < 10 :
+                day = '0' + str(j)
             else:
-                hour = str(i)
+                day = str(j)
 
-            #create a directory on the local home folder of the cluster
-            os.system('mkdir ' + month + '/' + day + '/' + hour)
-            
-            #fetcheds the data from the cluster and saves it in the folder created jsut before which is local on the cluster
-            os.system('scp -r -p cleres@iccluster028.iccluster.epfl.ch:/home/cleres/' + month + '/' + day + '/' + hour + '/*.json.bz2 ./' + month + '/' + day + '/' + hour)
+            os.system('mkdir ' + month + '/' + day)
 
+            for i in range(24):
+                if i < 10 :
+                    hour = '0' + str(i)
+                else:
+                    hour = str(i)
 
-            #keep only the english tweets
-            mypath = './' + month + '/' + day + '/' + hour + '/'
-
-            for f in listdir(mypath):
-                df = pd.read_json(mypath + f, lines=True)
-                df = df[df.lang=='en']
-                df = df.reset_index()
+                #create a directory on the local home folder of the cluster
+                os.system('mkdir ' + month + '/' + day + '/' + hour)
                 
-                print('SAVING TO :' , mypath + f)
-                df.to_json(mypath + f, compression='bz2')
+                #fetcheds the data from the cluster and saves it in the folder created jsut before which is local on the cluster
+                os.system('scp -r -p cleres@iccluster028.iccluster.epfl.ch:/home/cleres/' + month + '/' + day + '/' + hour + '/*.json.bz2 ./' + month + '/' + day + '/' + hour)
+
+
+                #keep only the english tweets
+                mypath = './' + month + '/' + day + '/' + hour + '/'
+
+                for f in listdir(mypath):
+                    df = pd.read_json(mypath + f, lines=True)
+                    df = df[df.lang=='en']
+                    df = df.reset_index()
+                    
+                    print('SAVING TO :' , mypath + f)
+                    df.to_json(mypath + f, compression='bz2')
